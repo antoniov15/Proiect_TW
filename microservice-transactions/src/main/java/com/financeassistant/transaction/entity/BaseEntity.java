021c6ab54e80d49c89106a2318dc1e68c97e454b
Laurentiu Tisca <lau572004@gmail.com>
2025-10-31 18:27:17 +0200

    [feat]: Add Transaction and Category repositories

--- microservice-transactions/src/main/java/com/financeassistant/transaction/entity/BaseEntity.java
+++ microservice-transactions/src/main/java/com/financeassistant/transaction/entity/BaseEntity.java
@@ -4,21 +4,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
 @MappedSuperclass // Spune JPA sa nu creeze o tabela pt clasa asta, ci sa foloseasca campurile in copiii ei
 public abstract class BaseEntity implements Serializable {
 
+    @Getter
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    // Getters si Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
---
+++ microservice-transactions/src/main/java/com/financeassistant/transaction/repository/TransactionRepository.java
@@ -0,0 +1,13 @@
+package com.financeassistant.transaction.repository;
+
+import com.financeassistant.transaction.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByUserId(Long userId);
+}
