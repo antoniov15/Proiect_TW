44fa6d1165ffd10ebfb678de53e0eba0326b1f12
Laurentiu Tisca <lau572004@gmail.com>
2025-11-25 14:59:54 +0200

    [feat]: Added Dockerfile to microservice-transactions and prepared it for eurekaserver

--- microservice-transactions/src/main/java/com/financeassistant/transaction/MicroserviceTransactionsApplication.java
+++ microservice-transactions/src/main/java/com/financeassistant/transaction/MicroserviceTransactionsApplication.java
@@ -2,8 +2,10 @@ package com.financeassistant.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceTransactionsApplication {
 
     public static void main(String[] args) {
