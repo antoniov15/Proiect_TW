fddcc21d2666c2531cff71accf96b32d6bd77121
Laurentiu Tisca <lau572004@gmail.com>
2025-11-25 16:18:07 +0200

    [fix]: Readded transactions and added AI as maven project

---
+++ microservice-transactions/src/main/java/com/financeassistant/transaction/config/DataInitializer.java
@@ -0,0 +1,39 @@
+package com.financeassistant.transaction.config;
+
+import com.financeassistant.transaction.entity.Category;
+import com.financeassistant.transaction.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public DataInitializer(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (categoryRepository.count() == 0) {
+            System.out.println("Populating categories table...");
+
+            Category category1 = new Category();
+            category1.setName("Salariu");
+            categoryRepository.save(category1);
+
+            Category category2 = new Category();
+            category2.setName("Cumparaturi");
+            categoryRepository.save(category2);
+
+            Category category3 = new Category();
+            category3.setName("Facturi");
+            categoryRepository.save(category3);
+
+            System.out.println("Finished populating categories.");
+        }
+    }
+}
