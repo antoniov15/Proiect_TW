b3b45ca26b2b4d14eae5feb8289dde2b2ec2458e
Antonio Samir Vicas <antonio.vicas@student.unitbv.ro>
2025-12-01 18:48:29 +0200

    feat: Add stored procedures for user management and corresponding tests

--- microservice-account/src/main/java/org/example/microserviceaccount/repository/AccountRepository.java
+++ microservice-account/src/main/java/org/example/microserviceaccount/repository/AccountRepository.java
@@ -2,8 +2,11 @@ package org.example.microserviceaccount.repository;
 
 import org.example.microserviceaccount.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.List;
 
@@ -11,6 +14,18 @@ import java.util.List;
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByEmail(String email);
     Optional<Account> findByUserName(String userName);
-
     List<Account> findByUserNameContainingIgnoreCase(String usernameFragment);
+
+    /// PROCEDURI STOCATE
+    // Count new users in a time frame
+    @Query(value = "SELECT account_schema.count_new_users(:startDate, :endDate)", nativeQuery = true)
+    Integer countNewUsers(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    // Anonymize user data
+    @Query(value = "SELECT account_schema.anonymizeUserData(:userId)", nativeQuery = true)
+    String anonymizeUserData(@Param("userId") Long userId);
+
+    // Check account availability
+    @Query(value = "SELECT account_schema.check_account_availability(:email, :username)", nativeQuery = true)
+    String checkAccountAvailability(@Param("email") String email, @Param("username") String username);
 }
