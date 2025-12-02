9768d6c26d412fbe37dc394ebed4ac4266e5bbfd
dan.ignat.ext <dan.ignat.ext@siemens.com>
2025-12-02 20:57:16 +0200

    changed security setting and added logout

--- gatewayserver/src/main/java/com/financeassistant/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/financeassistant/gatewayserver/config/SecurityConfig.java
@@ -2,57 +2,80 @@ package com.financeassistant.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcReactiveOAuth2UserService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
+import java.net.URI;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    private final List<String> adminEmails = List.of("laurentiu.danciu@student.unitbv.ro", "antonio.vizitiu@student.unitbv.ro", "daniel.ignat@student.unitbv.ro");
+    private static final List<String> ADMIN_EMAILS = List.of(
+            "laurentiu@student.unitbv.ro",
+            "antonio@student.unitbv.ro",
+            "dan.ignat@student.unitbv.ro"
+    );
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/", "/index.html", "/login", "/oauth2/**", "/actuator/**").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .oauth2Login(Customizer.withDefaults())
-                .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
-        return http.build();
+        RedirectServerLogoutSuccessHandler logoutSuccessHandler =
+                new RedirectServerLogoutSuccessHandler();
+        logoutSuccessHandler.setLogoutSuccessUrl(URI.create("/"));
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                                .pathMatchers("/", "/index.html", "/login", "/oauth2/**", "/actuator/**").permitAll()
+                                .anyExchange().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .logout(logout -> logout
+                                .logoutUrl("/logout")
+
+                                .requiresLogout(ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/logout"))
+                                .logoutSuccessHandler(logoutSuccessHandler)
+                )
+                .build();
     }
 
     @Bean
     public ReactiveOAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
-        final OidcReactiveOAuth2UserService delegate = new OidcReactiveOAuth2UserService();
+        OidcReactiveOAuth2UserService delegate = new OidcReactiveOAuth2UserService();
 
         return userRequest -> delegate.loadUser(userRequest).map(oidcUser -> {
             Set<GrantedAuthority> authorities = new HashSet<>(oidcUser.getAuthorities());
             String email = oidcUser.getAttribute("email");
 
-            if (adminEmails.contains(email)) {
+            if (email != null && ADMIN_EMAILS.contains(email)) {
                 authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
             } else {
                 authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
             }
 
-            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+            return new DefaultOidcUser(
+                    authorities,
+                    oidcUser.getIdToken(),
+                    oidcUser.getUserInfo(),
+                    "email"
+            );
         });
     }
 }
-
