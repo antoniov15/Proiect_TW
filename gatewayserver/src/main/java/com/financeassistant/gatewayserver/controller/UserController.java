e79c3ad9e687905fca5c92aab8b9c08468f15214
dan.ignat.ext <dan.ignat.ext@siemens.com>
2025-12-02 17:00:09 +0200

    added a small user controller to check authentification

---
+++ gatewayserver/src/main/java/com/financeassistant/gatewayserver/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.financeassistant.gatewayserver.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public Mono<Map<String, Object>> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Mono.just(principal.getAttributes());
+    }
+
+    @GetMapping("/")
+    public Mono<String> home(@AuthenticationPrincipal OAuth2User principal) {
+        return Mono.just("Welcome " + principal.getAttribute("name"));
+    }
+}
