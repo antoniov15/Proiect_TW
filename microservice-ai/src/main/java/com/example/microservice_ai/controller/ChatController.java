5cb18aa96d0d37225420eafa3f1a037c81a25540
dan.ignat.ext <dan.ignat.ext@siemens.com>
2025-12-04 12:21:07 +0200

    added /api root in chat controller

--- microservice-ai/src/main/java/com/example/microservice_ai/controller/ChatController.java
+++ microservice-ai/src/main/java/com/example/microservice_ai/controller/ChatController.java
@@ -1,25 +1,33 @@
 package com.example.microservice_ai.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microservice_ai.dto.ChatCreateDTO;
+import com.example.microservice_ai.dto.ChatDTO;
+import com.example.microservice_ai.dto.MessageCreateDTO;
+import com.example.microservice_ai.dto.MessageDTO;
+import com.example.microservice_ai.service.IChatService;
+
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import com.example.microservice_ai.dto.MessageCreateDTO;
-import com.example.microservice_ai.dto.MessageDTO;
-import com.example.microservice_ai.dto.ChatCreateDTO;
-import com.example.microservice_ai.dto.ChatDTO;
-import com.example.microservice_ai.service.IChatService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
-import java.util.List;
 
 @RestController
-@RequestMapping(path="/chats")
+@RequestMapping(path="/api/chats")
 public class ChatController {
 	@Autowired
 	private IChatService chatService;
