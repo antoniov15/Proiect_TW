47f830ea009d3f223f6a42d7163466a12b2b6729
Laurentiu Tisca <lau572004@gmail.com>
2025-12-10 19:46:55 +0200

    []: Part of IamRoles implementation

--- gatewayserver/src/main/java/com/financeassistant/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/financeassistant/gatewayserver/config/SecurityConfig.java
@@ -1,8 +1,20 @@
 package com.financeassistant.gatewayserver.config;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.cloudresourcemanager.CloudResourceManager;
+import com.google.api.services.cloudresourcemanager.model.Binding;
+import com.google.api.services.cloudresourcemanager.model.GetIamPolicyRequest;
+import com.google.api.services.cloudresourcemanager.model.Policy;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,73 +25,145 @@ import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserSer
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.net.URI;
+import java.security.GeneralSecurityException;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebFluxSecurity
+@Profile("test")
 public class SecurityConfig {
 
-    private static final List<String> ADMIN_EMAILS = List.of(
-            "laurentiu@student.unitbv.ro",
-            "antonio@student.unitbv.ro",
-            "dan.ignat@student.unitbv.ro",
-//            "lau572004@gmail.com",
-            "antoniox2004.av@gmail.com"
-    );
+    //TODO: set your GCP project ID
+    private final String idProject = "";
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-
-        RedirectServerLogoutSuccessHandler logoutSuccessHandler =
-                new RedirectServerLogoutSuccessHandler();
-        logoutSuccessHandler.setLogoutSuccessUrl(URI.create("/"));
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
 
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/", "/index.html", "/login", "/oauth2/**", "/actuator/**").permitAll()
+                        .pathMatchers("/", "/index.html", "/login/**", "/oauth2/**", "/actuator/**").permitAll()
+
                         .pathMatchers(HttpMethod.DELETE, "/microservice-transactions/**").hasRole("ADMIN")
-                        .pathMatchers("/microservice-transactions/**").authenticated()
                         .anyExchange().authenticated()
                 )
-                .oauth2Login(withDefaults())
-                .logout(logout -> logout
-                                .logoutUrl("/logout")
-
-                                .requiresLogout(ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/logout"))
-                                .logoutSuccessHandler(logoutSuccessHandler)
+                .oauth2Login(oauth2 -> oauth2
+                        .authenticationSuccessHandler(successHandler())
                 )
+                .oauth2Client(Customizer.withDefaults())
                 .build();
     }
 
     @Bean
+    public ServerAuthenticationSuccessHandler successHandler() {
+        return (webFilterExchange, authentication) -> {
+            System.out.println("Authenticated authorities at success: " + authentication.getAuthorities());
+
+            webFilterExchange
+                    .getExchange()
+                    .getResponse()
+                    .setStatusCode(org.springframework.http.HttpStatus.FOUND);
+
+            webFilterExchange
+                    .getExchange()
+                    .getResponse()
+                    .getHeaders().set("Location", "http://localhost:8072/university");
+
+            return webFilterExchange.getExchange().getResponse().setComplete();
+        };
+    }
+
+    /*
+    OIDC (OpenID Connect)
+    Allows the client to know who the user is and get user information.
+    Uses ID Tokens (JWTs) to carry user identity information
+     */
+    @Bean
     public ReactiveOAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
         OidcReactiveOAuth2UserService delegate = new OidcReactiveOAuth2UserService();
 
-        return userRequest -> delegate.loadUser(userRequest).map(oidcUser -> {
-            Set<GrantedAuthority> authorities = new HashSet<>(oidcUser.getAuthorities());
-            String email = oidcUser.getAttribute("email");
+        return new ReactiveOAuth2UserService<OidcUserRequest, OidcUser>() {
+            @Override
+            public Mono<OidcUser> loadUser(OidcUserRequest userRequest) {
+                return delegate.loadUser(userRequest)
+                        .map(oidcUser -> {
+                            Set<GrantedAuthority> mappedAuthorities = new HashSet<>(oidcUser.getAuthorities());
 
-            if (email != null && ADMIN_EMAILS.contains(email)) {
-                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            } else {
-                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+                            try {
+                                Set<GrantedAuthority> iamRoles = getIamRoles(userRequest, oidcUser);
+                                mappedAuthorities.addAll(iamRoles);
+
+                            }catch (GeneralSecurityException | IOException e) {
+                                System.out.println(e.getMessage());
+                            }
+
+                            System.out.println("Mapped authorities: " + mappedAuthorities);
+
+                            return new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+                        });
             }
-
-            return new DefaultOidcUser(
-                    authorities,
-                    oidcUser.getIdToken(),
-                    oidcUser.getUserInfo(),
-                    "email"
-            );
-        });
+        };
     }
+
+    private Set<GrantedAuthority> getIamRoles(OidcUserRequest userRequest, OidcUser oidcUser) throws GeneralSecurityException, IOException {
+
+        String accessTokenValue = userRequest.getAccessToken().getTokenValue();
+        System.out.println("accessTokenValue: " + accessTokenValue);
+
+        AccessToken accessToken = new AccessToken(accessTokenValue, Date.from(userRequest.getAccessToken().getExpiresAt()));
+        System.out.println("accessToken: " + accessToken.getTokenValue());
+
+        GoogleCredentials credentials = GoogleCredentials.create(accessToken);
+        System.out.println("credentials: " + credentials);
+
+        CloudResourceManager handler = new CloudResourceManager.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                GsonFactory.getDefaultInstance(),
+                new HttpCredentialsAdapter(credentials))
+                .setApplicationName("GatewayServer")
+                .build();
+
+        GetIamPolicyRequest policyRequest = new GetIamPolicyRequest();
+        Policy policy =  handler.projects().getIamPolicy(idProject, policyRequest).execute();
+        System.out.println("policy: " + policy);
+
+        String email = oidcUser.getEmail();
+        String identifier = "user:" + email;
+
+        return policy.getBindings().stream()
+                .filter(binding -> binding.getMembers() != null && binding.getMembers().contains(identifier))
+                .map(Binding::getRole)
+                .peek(role -> System.out.println("Role is: " + role))
+                .map(this::mapIamRolesToApplicationRoles)
+                .collect(Collectors.toSet());
+
+    }
+
+    private GrantedAuthority mapIamRolesToApplicationRoles(String role) {
+
+        if ("roles/owner".equals(role)) {
+            return new SimpleGrantedAuthority("ROLE_ADMIN");
+        }
+        if ("roles/editor".equals(role)) {
+            return new SimpleGrantedAuthority("ROLE_USER");
+        }
+        if ("roles/viewer".equals(role)) {
+            return new SimpleGrantedAuthority("ROLE_USER");
+        }
+        return new SimpleGrantedAuthority("ROLE_GUEST");
+    }
+
+
 }
